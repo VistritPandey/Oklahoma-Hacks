85e974e0bfbec20c1d6977b0a8d7e2ce4787235f
vistritpandey <vistritpandey@gmail.com>
2021-02-07 07:54:04 +0530

    Icons added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
