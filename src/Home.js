547bacd0d6e7adef2b2392d094461a947fe8f6f9
vistritpandey <vistritpandey@gmail.com>
2021-02-07 07:24:38 +0530

    Home added

---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import { Avatar } from "@material-ui/core";
+import Search from "./Search";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerleft"></div>
+        <div className="home__headerright">
+          <Link to="/gmail">Gmail</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+        <div className="home__inputcontainer" />
+        <Search hideButtons />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
